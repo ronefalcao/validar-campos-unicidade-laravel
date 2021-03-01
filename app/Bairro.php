23109ccac3ef020497e98fe9e85214cfaf731f54
Rone Falcão <63078068+mirterrone@users.noreply.github.com>
2021-03-01 14:09:13 -0300

    Update Bairro.php

--- app/Bairro.php
+++ app/Bairro.php
@@ -17,16 +17,16 @@ class Bairro extends Model
     ];
 
 
-    public function validacao($data){
+    public function validacao($data, $id){
         
-        $inscricao = $data['bairro_nome'];
-        $cidade_id = isset($data['cidade_id']) ? $data['cidade_id'] : 0;
+        $inscricao = $data['bairro_nome'] ?? "";
+        $cidade_id = $data['cidade_id'] ?? 0;
         $regras = [
             'bairro_nome' => ['required', Rule::unique($this->table, 'bairro_nome')
                 ->where(function ($query) use ($inscricao, $cidade_id) {
                     $query->where('bairro_nome', $inscricao);
                     $query->where('cidade_id', $cidade_id);
-                })],
+                })->ignore($id, 'cod_bairro')],
             'cidade_id' => 'required',
              
         ];
@@ -40,28 +40,6 @@ class Bairro extends Model
         return Validator::make($data, $regras, $mensagens);
     }
 
-    public function validacaoUpdate($data, $id)
-    {
-        $inscricao = $data['bairro_nome'];
-        $cidade_id = isset($data['cidade_id']) ? $data['cidade_id'] : 0;
-
-        $regras = [
-            'bairro_nome' => ['required', Rule::unique($this->table, 'bairro_nome')
-                ->where(function ($query) use ($inscricao, $cidade_id) {
-                    $query->where('bairro_nome', $inscricao);
-                    $query->where('cidade_id', $cidade_id);
-                })->ignore($id, 'bairro_id')],
-            'cidade_id' => 'required',
-             
-        ];
-        $mensagens = [
-            'bairro_nome.required' => 'Bairro é obrigatório!',
-            'bairro_nome.unique' => 'Bairro ja esta cadastrado!',
-            'cidade_id.required' => 'Cidade é obrigatório!',
-            
-
-        ];
-        return Validator::make($data, $regras, $mensagens);
-    }
+     
 
 }
